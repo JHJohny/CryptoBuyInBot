3aacc7cff5541fcf52b4e64a456c5b5ebfb57873
JHJohny <ondis.jan1@gmail.com>
2019-11-28 19:29:20 +0100

    Added argument decorator

--- utils/decorator/Decorators.py
+++ utils/decorator/Decorators.py
@@ -1,6 +1,7 @@
 from utils.Signatures import get_kwd_args #For function signature
 
 def block_none_or_empty_arguments(func):
+    """Protect of overwritting default arguments to set none or empty"""
     default_keyword = get_kwd_args(func) #Store signature keyword arguments
 
     def wrapper(*args, **kwargs):
@@ -13,3 +14,21 @@ def block_none_or_empty_arguments(func):
 
         return func(*args, **kwargs)
     return wrapper
+
+
+def block_argument(*block_args):
+    """Protect whatever type of argument to overwrite default arguments in function"""
+    def block_args(func):
+        default_keyword = get_kwd_args(func) #Store signature keyword arguments
+
+        def wrapper(*args, **kwargs):
+
+            #Check if some of inserted arguments are empty
+            for key in kwargs:
+                if kwargs[key] in block_args:
+                    if key in default_keyword:
+                        kwargs[key] = default_keyword[key]
+
+            return func(*args, **kwargs)
+        return wrapper
+    return block_args
